33efe3eedc05256228c0c392bce6bab821c8fb78
孙榜 <sunbang_0@qq.com>
2020-10-29 16:01:58 +0800

    git第一次提交测试

---
+++ src/main/java/com/nbufe/ben/BenApplication.java
@@ -0,0 +1,13 @@
+package com.nbufe.ben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenApplication.class, args);
+	}
+
+}
